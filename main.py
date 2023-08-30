e8f630d8bfe391bc048af0084df332d929464597
omarm <omarmourad9119@gmail.com>
2023-08-29 23:45:26 -0400

    Day 26: Add header, total price, company and logo

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ for filepath in filepaths:
     columns = list(df.columns)
     # use a list comprehension
     columns = [item.replace("_", " ").title() for item in columns]
-    pdf.set_font(family="Times", size=10, style="B" )
+    pdf.set_font(family="Times", size=10, style="B")
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
@@ -34,11 +34,28 @@ for filepath in filepaths:
 
     # add rows
     for index, row in df.iterrows():
-        pdf.set_font(family="Times", size=10,)
+        pdf.set_font(family="Times", size=10, )
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=70, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10, )
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=25, h=8, txt="PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
